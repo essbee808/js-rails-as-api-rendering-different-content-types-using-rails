65959f69df48b69049e79664285e19055ba97934
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-28 17:57:19 -0700

    commit tree 38f828adff78a9f35271e14bbd09abe89774af18

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,10 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    # no longer need instance variables since instance variables were used to render ERB
+    # @birds = Bird.all
+    #render plain: "Hello #{@birds[3].name}"
+    #render json: @birds
+    birds = Bird.all
+    render json: birds
   end
 end
